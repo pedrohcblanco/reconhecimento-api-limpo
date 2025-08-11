b042ffb8a99bf9b2fe4111c270ec6360f401cc27
pedrohcblanco <pedro.blanco@inpel.com.br>
2025-08-11 10:31:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import os
 
 app = Flask(__name__)
 
@@ -40,4 +41,5 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
